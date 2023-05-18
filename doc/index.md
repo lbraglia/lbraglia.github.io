f2006c71942556c43b2d4d89ad8608e7c1b03e44
Luca Braglia <lbraglia@gmail.com>
2023-05-18 12:31:17 +0200

    update

--- doc/index.md
+++ doc/index.md
@@ -1,19 +1,21 @@
-misc/
+# doc
 
--    [canzoniere.pdf](misc/canzoniere.pdf)
--    [ricettario.pdf](misc/ricettario.pdf)
+## misc 
 
-
-notes/
-
--    [sql.pdf](notes/sql.pdf)
--    [r.pdf](notes/r.pdf)
--    [python.pdf](notes/python.pdf)
--    [c.pdf](notes/c.pdf)
+- [canzoniere.pdf](misc/canzoniere.pdf)
+- [ricettario.pdf](misc/ricettario.pdf)
 
 
-dae/
+## notes 
 
--    [test.txt](dae/test.txt)
+- [sql.pdf](notes/sql.pdf)
+- [r.pdf](notes/r.pdf)
+- [python.pdf](notes/python.pdf)
+- [c.pdf](notes/c.pdf)
+
+
+## dae 
+
+- [test.txt](dae/test.txt)
 
 
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
+import sys
+
 
 # https://stackoverflow.com/questions/9727673/ with little changes
-def list_files(startpath = "doc"):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep) - 1 
-        if level == -1: # non mostrare doc
-            continue
-        indent = ' ' * 4 * (level)
-        print('{}{}/\n'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('-{}[{}]({})'.format(
-                subindent, f,
-                os.path.join(os.path.basename(root), f)))
-        print("\n")
-            
+def print_docindex(outfile = 'doc/index.md'):
+
+    original_stdout = sys.stdout
+    with open(outfile, 'w') as f:
+        # redirect standard output to file
+        sys.stdout = f
+        print("# doc\n")
+        for root, dirs, files in os.walk("doc"):
+            level = root.replace("doc", '').count(os.sep) - 1 
+            if level == -1: # non mostrare doc
+                continue
+            # print directory as subtitle
+            print("## {0} \n".format(os.path.basename(root)))
+            # print file and links
+            for f in files:
+                print('- [{}]({})'.format(f, os.path.join(os.path.basename(root), f)))
+            print("\n")
+    sys.stdout = original_stdout
             
 if __name__ == '__main__':
-    list_files()
+    print_docindex()
